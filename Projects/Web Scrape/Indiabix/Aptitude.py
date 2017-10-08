d904ce7717dd8cf86f5e3f9b4953d38269c65e8f
Vinay Narayana <vinay@programmer.net>
2017-10-08 21:28:28 +0530

    Gather all the Links of Aptitude section
    all the links finally copied into the AptiLinks.txt file which will be preprocessed by other AptiQ&A.py script :)

---
+++ Projects/Web Scrape/Indiabix/Aptitude.py
@@ -0,0 +1,52 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import os
+
+f = open('AptiLinks.txt','w+')
+links = []
+
+def link():
+    url = r'https://www.indiabix.com/aptitude/questions-and-answers/'
+    r = requests.get(url).text
+    soup = bs(r,'html.parser')
+    for x in soup.find_all('div',attrs={'class':'div-topics-index'}):
+        for y in x.find_all('a'):
+            links.append('https://www.indiabix.com'+y['href'])
+            print links[-1]
+    return links
+
+singlePage = []
+subPages = []
+def EpageMax(url):
+    r = requests.get(url).text
+    soup = bs(r,'html.parser')
+    for x in soup.find_all('p',attrs={'class':'mx-pager mx-lpad-25'}):
+        for y in x.find_all('a'):
+            if 'Next' not in y.get_text():
+                singlePage.append('https://www.indiabix.com'+y['href'])
+                print singlePage[-1]+' ',
+                f.write(singlePage[-1]+'\n')
+                print y.get_text()
+
+
+def subPage(links):
+    for url in links:
+        print url
+        r = requests.get(url).text
+        soup = bs(r,'html.parser')
+        f.write(url+'\n')
+        EpageMax(url)
+        for x in soup.find_all('ul',attrs={'class':'ul-top-left'}):
+            for y in x.find_all('a'):
+                subPages.append('https://www.indiabix.com'+y['href'])
+                print subPages[-1]+' ',
+                print y.get_text()
+                f.write(subPages[-1]+'\n')
+                EpageMax(subPages[-1])
+
+  
+
+#link()
+subPage(link())
+f.close()
+#EpageMax(link())
